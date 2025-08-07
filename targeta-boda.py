bb8f8b99af1ec9e183beb932c90a8c05392b2f80
Hans-developer <dev.hans.saldias@gmail.com>
2025-08-07 14:46:29 -0400

    Update targeta-boda.py

--- targeta-boda.py
+++ targeta-boda.py
@@ -18,8 +18,29 @@ def guardar_datos(nombre):
     df = df.append({"Nombre": nombre}, ignore_index=True)
     df.to_csv(CSV_FILE, index=False)
 
+# Sidebar para iniciar sesión
+st.sidebar.title("Iniciar Sesión")
+usuario = st.sidebar.text_input("Usuario")
+contrasena = st.sidebar.text_input("Contraseña", type="password")
+
+# Verifica las credenciales
+if st.sidebar.button("Iniciar sesión"):
+    if usuario == "admin" and contrasena == "admin123":  # Cambia las credenciales según sea necesario
+        st.sidebar.success("Inicio de sesión exitoso.")
+        
+        # Mostrar lista de asistentes
+        st.sidebar.markdown("### Lista de asistentes:")
+        asistentes = cargar_datos()
+        if not asistentes.empty:
+            st.sidebar.write(asistentes)
+            st.sidebar.write(f"Total de invitados registrados: {len(asistentes)}")
+        else:
+            st.sidebar.write("No hay asistentes confirmados todavía.")
+    else:
+        st.sidebar.error("Usuario o contraseña incorrectos.")
+
 # Título de la aplicación
-st.title("Invitación de Boda")
+st.title("Registro para la Boda")
 
 # Información de la boda
 st.header("¡Estás cordialmente invitado a nuestra boda!")
@@ -31,29 +52,10 @@ st.markdown("### Por favor, ingresa tu nombre:")
 nombre_invitado = st.text_input("Nombre del invitado")
 
 # Botón para confirmar asistencia
-if st.button("Confirmar asistencia"):
+if st.button("Registrar asistencia"):
     if nombre_invitado:
         guardar_datos(nombre_invitado)
-        st.success(f"{nombre_invitado}, tu asistencia ha sido confirmada.")
+        st.success("Registro exitoso, ¡nos vemos en mi boda! Posdata: Marcela")
+        st.experimental_rerun()  # Limpiar los campos y reiniciar la aplicación
     else:
         st.error("Por favor, ingresa tu nombre.")
-
-# Sección de inicio de sesión para el administrador
-st.markdown("### Área de Administrador")
-usuario = st.text_input("Usuario")
-contrasena = st.text_input("Contraseña", type="password")
-
-# Verifica las credenciales
-if st.button("Iniciar sesión"):
-    if usuario == "admin" and contrasena == "admin123":  # Cambia las credenciales según sea necesario
-        st.success("Inicio de sesión exitoso.")
-        
-        # Mostrar lista de asistentes
-        st.markdown("### Lista de asistentes:")
-        asistentes = cargar_datos()
-        if not asistentes.empty:
-            st.write(asistentes)
-        else:
-            st.write("No hay asistentes confirmados todavía.")
-    else:
-        st.error("Usuario o contraseña incorrectos.")
